6818a87b28009e0f6db740b5c8f482e1c28f5319
hanumantjain <hanumantjain939@gmail.com>
2025-06-13 18:28:51 -0400

    Login and register

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { loginUser } from '../api/auth';
 import { useAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
 
 export default function Login() {
   const { setToken } = useAuth();
@@ -12,17 +14,56 @@ export default function Login() {
     try {
       const res = await loginUser({ email, password });
       setToken(res.data.token);
-      alert('Login successful!');
+      
     } catch (err: any) {
       alert(err.response?.data?.message || 'Login failed');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
+    <div className='pt-20'>
+      <div className='flex flex-col justify-between items-center text-lg gap-10'>
+        <h1 className='text-2xl'>Sign in your account</h1>
+        <div className='w-1/3 border p-14 flex flex-col gap-8 rounded'>
+          <div className=''>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
+              <div className='flex flex-col gap-2'>
+                <label>Email address</label>
+                <input 
+                  type="text" 
+                  name=""
+                  value={email}
+                  onChange={e => setEmail(e.target.value)} 
+                  className='border p-2 rounded'/>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label>Password</label>
+                <input 
+                  type="password" 
+                  name=""
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  className='border p-2 rounded' />
+              </div>
+              <div className='flex justify-end'>
+                <p>Forgot password?</p>
+              </div>
+              <button type='submit' className='border rounded w-full py-2'>Sign in</button>
+            </form>
+          </div>
+          <h1>Or continue with</h1>
+          <div className='flex justify-center items-center'>
+            <button className='flex justify-center items-center gap-2 border rounded w-full py-2'>
+               <FcGoogle />
+               Google</button>
+          </div> 
+        </div>
+        <h1>Don't have account?{' '}
+          <Link to='/register' className='underline'>
+            Register
+          </Link>
+        </h1>
+      </div>
+    </div>
   );
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,26 +1,92 @@
 import { useState } from 'react';
 import { registerUser } from '../api/auth';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
 
 export default function Register() {
   const [form, setForm] = useState({ firstName: '', lastName: '', email: '', password: '' });
+  const[confirmPassword, setComfirmPassword] = useState('')
+  const [error, setError] = useState('')
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if(form.password !== confirmPassword){
+      setError("Passwords do not match")
+    } else{
+      setError('')
+    }
     try {
       await registerUser(form);
-      alert('Registered successfully. You can now log in.');
     } catch (err: any) {
-      alert(err.response?.data?.message || 'Registration failed');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder="First Name" onChange={e => setForm({ ...form, firstName: e.target.value })} />
-      <input placeholder="Last Name" onChange={e => setForm({ ...form, lastName: e.target.value })} />
-      <input placeholder="Email" onChange={e => setForm({ ...form, email: e.target.value })} />
-      <input type="password" placeholder="Password" onChange={e => setForm({ ...form, password: e.target.value })} />
-      <button type="submit">Register</button>
-    </form>
+    <div className='pt-20'>
+      <div className='flex flex-col justify-between items-center text-lg gap-10'>
+        <h1 className='text-2xl'>Register your account</h1>
+        <div className='w-1/3 border p-14 flex flex-col gap-8 rounded'>
+          <div className=''>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
+              <div className='flex gap-4'>
+                <div className='flex flex-col gap-2'>
+                  <label>First Name</label>
+                  <input 
+                    type="text" 
+                    onChange={e => setForm({ ...form, firstName: e.target.value })}
+                    className='border p-2 rounded w-full'
+                    required />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label>Last Name</label>
+                  <input 
+                    type="text" 
+                    onChange={e => setForm({ ...form, lastName: e.target.value })}
+                    className='w-full border p-2 rounded'
+                    required />
+                </div>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label>Email address</label>
+                <input 
+                  type="text" 
+                  onChange={e => setForm({ ...form, email: e.target.value })}
+                  className='border p-2 rounded'
+                  required />
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label>Password</label>
+                <input 
+                  type="password" 
+                  onChange={e => setForm({ ...form, password: e.target.value })}
+                  className='border p-2 rounded' 
+                  required />
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label>Confirm Password</label>
+                <input 
+                  type="password" 
+                  onChange={e => setComfirmPassword(e.target.value)}
+                  className='border p-2 rounded'
+                  required />
+              </div>
+              {error && <p className='text-red-500'>{error}</p>}
+              <button type='submit' className='border rounded w-full py-2'>Register</button>
+            </form>
+          </div>
+          <h1>Or continue with</h1>
+          <div className='flex justify-center items-center'>
+            <button className='flex justify-center items-center gap-2 border rounded w-full py-2'>
+               <FcGoogle />
+               Google</button>
+          </div> 
+        </div>
+        <h1>Have an account?{' '}
+          <Link to='/login' className='underline'>
+            Sign in
+          </Link>
+        </h1>
+      </div>
+    </div>
   );
 }
